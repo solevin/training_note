eb09654b75340d9f1b1a5178d33a1791ba676101
taiyo78910 <y.kambayashi78910@gmail.com>
2022-06-12 17:15:28 +0900

    #3:練習記録入力フォーム作成

--- lib/calendar/calendar_page.dart
+++ lib/calendar/calendar_page.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:collection';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:training_note/calendar/training_log_page.dart';
+import 'package:training_note/calendar/calendar_page_view.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final focusProvider = StateProvider<DateTime>((ref) => DateTime.now());
-final selectedProvider = StateProvider<DateTime>((ref) => DateTime.now());
 
 Widget calendarPage(BuildContext context, WidgetRef ref) {
   DateTime focusedDay = ref.watch(focusProvider);
   DateTime selectedDay = ref.watch(selectedProvider);
+  Map<DateTime, List> preEvents = ref.watch(preEventProvider);
+  final tmp = {
+    DateTime.now().subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+    DateTime.now(): ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+    DateTime.now().add(Duration(days: 1)): [
+      'Event A8',
+      'Event B8',
+      'Event C8',
+      'Event D8'
+    ],
+    DateTime.now().add(Duration(days: 3)):
+        Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+  };
+  ref.read(preEventProvider.notifier).state.addAll(tmp);
+
+  LinkedHashMap<DateTime, List> events = LinkedHashMap<DateTime, List>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  )..addAll(preEvents);
+
+  List getEventForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
   return Column(
     children: [
       Padding(
@@ -18,6 +41,7 @@ Widget calendarPage(BuildContext context, WidgetRef ref) {
           firstDay: DateTime.utc(2020, 1, 1),
           lastDay: DateTime.utc(2050, 12, 31),
           focusedDay: focusedDay,
+          eventLoader: getEventForDay,
           headerStyle: const HeaderStyle(
             formatButtonVisible: false,
           ),
@@ -27,15 +51,34 @@ Widget calendarPage(BuildContext context, WidgetRef ref) {
           onDaySelected: (newSelectedDay, newFocusedDay) {
             if (!isSameDay(selectedDay, newSelectedDay)) {
               ref.read(selectedProvider.notifier).state = newSelectedDay;
-              // ref.read(focusProvider.notifier).state = newFocusedDay;
+              ref.read(focusProvider.notifier).state = newFocusedDay;
+              getEventForDay(selectedDay);
             } else {
               Navigator.of(context).push<dynamic>(
                 TrainingLogPage.route(),
               );
+              // ref.read(preEventProvider.notifier).state.addAll({
+              //   selectedDay: ['test']
+              // });
             }
+          },
+          onPageChanged: (newFocusedDay) {
+            ref.read(focusProvider.notifier).state = newFocusedDay;
           },
         ),
       ),
+      ListView(
+        shrinkWrap: true,
+        children: getEventForDay(selectedDay)
+            .map((event) => ListTile(
+                  title: Text(event.toString()),
+                ))
+            .toList(),
+      )
     ],
   );
+}
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
 }
--- lib/calendar/training_log_page.dart
+++ lib/calendar/training_log_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:training_note/calendar/calendar_page_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 final indexProvider = StateProvider<int>((ref) => 0);
 
@@ -14,13 +16,153 @@ class TrainingLogPage extends HookConsumerWidget {
   const TrainingLogPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    DateTime selectedDay = ref.watch(selectedProvider);
+    List<bool> isSelected = ref.watch(isSelectedProvider);
+    final pageTitle = Text(DateFormat('M/d (E)').format(selectedDay));
     return Scaffold(
-      body: Container(
-        child: Text(
-          'TrainingLog',
-          style: TextStyle(fontSize: 20.sp),
-        ),
+      appBar: AppBar(title: pageTitle),
+      body: Column(
+        children: [
+          isPractice(ref, isSelected),
+          isSelected[0] == true ? ballQuantity() : score(),
+          memo(),
+        ],
       ),
     );
   }
+}
+
+Widget isPractice(WidgetRef ref, List<bool> isSelected) {
+  return Padding(
+    padding: EdgeInsets.all(10.r),
+    child: Center(
+      child: ToggleButtons(
+        isSelected: isSelected,
+        onPressed: (index) {
+          List<bool> tmpList = [];
+          for (int i = 0; i < isSelected.length; i++) {
+            if (index == i) {
+              tmpList.add(true);
+            } else {
+              tmpList.add(false);
+            }
+          }
+          ref.read(isSelectedProvider.notifier).state = tmpList;
+        },
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.r),
+            child: Text(
+              '練習',
+              style: TextStyle(fontSize: 20.sp),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.r),
+            child: Text(
+              'コース',
+              style: TextStyle(fontSize: 20.sp),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget memo() {
+  return Column(
+    children: [
+      Text(
+        'メモ',
+        style: TextStyle(fontSize: 15.sp),
+      ),
+      Padding(
+        padding: EdgeInsets.fromLTRB(10.w, 0, 10.w, 0),
+        child: TextField(
+          style: TextStyle(fontSize: 15.sp),
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          decoration: const InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget ballQuantity() {
+  return Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '球数 : ',
+            style: TextStyle(fontSize: 20.sp),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10.w, 0, 10.w, 0),
+            child: SizedBox(
+              width: 80.w,
+              child: TextField(
+                style: TextStyle(fontSize: 15.sp),
+                keyboardType: TextInputType.number,
+                // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget score() {
+  return Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'スコア : ',
+            style: TextStyle(fontSize: 20.sp),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10.w, 0, 10.w, 0),
+            child: SizedBox(
+              width: 80.w,
+              child: TextField(
+                style: TextStyle(fontSize: 15.sp),
+                keyboardType: TextInputType.number,
+                // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
 }
